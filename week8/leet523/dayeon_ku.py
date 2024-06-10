e6ecf2e07f59a98cbbadad1884dcee7161a739a1
Dayeon <dayeonkuhk@gmail.com>
2024-06-10 23:56:14 +0900

    [다연] Continuous Subarray Sum, Subarray Sums Divisible by K

---
+++ week8/leet523/dayeon_ku.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        hash_map = {0:0}
+        s = 0
+
+        for i in range(len(nums)):
+            s += nums[i]
+
+            if s % k not in hash_map:
+                hash_map[s % k] = i + 1
+            elif hash_map[s % k] < i:
+                return True
+
+        return False
